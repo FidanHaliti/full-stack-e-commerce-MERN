6478fceed82e70ccfdab13dd2fac8e36f705ad09
Fidan Haliti <fidan91haliti@gmail.com>
2024-02-23 17:52:01 +0100

    process.env.PORT || 3000 ADD

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const app = express();
 const cors = require("cors");
 const logger = require("morgan");
 const mainRoute = require("./routes/index.js");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 dotenv.config();
 
